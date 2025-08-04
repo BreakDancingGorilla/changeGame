ef0a7e1c88c02116711c439f88470caaeaa9606c
BreakDancingGorilla <gabrielosborn1029@gmail.com>
2025-08-04 12:41:28 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -142,6 +142,7 @@ startingW: document.getElementById("salesbox").offsetWidth,
 startingH: this.billH,
 billList: [],
 change: 0,
+coinList: [],
 updateSlots: function(){
       ctx.clearRect(0,0,canvas.width,canvas.height);
   let j = 0;
@@ -165,111 +166,289 @@ newBillAmt: function(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
   this.billAmount = ranNum(Math.ceil(salesPrice.salesPrice),100);
 },
-
+/* (trimmedWallet[maxIndex] == 100) {
+   this.billList[this.billList.length] = "hundredDollar.png.png";
+   num-=100;
+   this.billAmount+=100;
+}
+if (trimmedWallet[maxIndex] == 50) {
+   this.billList[this.billList.length] = "fiftyDollar.png";
+   i+=50;
+    this.billAmount+=100;
+}
+if (trimmedWallet[maxIndex] == 20) {
+   this.billList[this.billList.length] = "twentyDollar.png";
+   i+=20;
+   this.billAmount+=20;
+}
+if (trimmedWallet[maxIndex] == 10) {
+   this.billList[this.billList.length] = "tenDollar.png";
+   i+=10;
+   this.billAmount+=10;
+}
+if (trimmedWallet[maxIndex] == 5) {
+   this.billList[this.billList.length] = "fiveDollar.png";
+   i+=5;
+   this.billAmount+=5;
+}
+if (trimmedWallet[maxIndex] == 1) {
+   this.billList[this.billList.length] = "oneDollar.png";
+   i+=1; */
 renderBills: function(){
   let wallet = [];
   let selection = 0;
   let maxWallet = this.billAmount;
   this.billList = [];
-  for (let i = 0; i <= maxWallet;) {
-    selection = ranNum(0,100);
-    if (selection > 0 && selection < 5) {
-      wallet[wallet.length] = 100;
-      i+=100;
-    }
-    if (selection > 5 && selection < 15) {
-      wallet[wallet.length] = 50;
-      i+=50;
-    }
-    if (selection > 15 && selection < 40) {
-      wallet[wallet.length] = 20;
-      i+=20;
-    }
-    if (selection > 40 && selection < 65) {
-      wallet[wallet.length] = 10;
-      i+=10;
-    }
-    if (selection > 65 && selection < 80) {
-      wallet[wallet.length] = 5;
-      i+=5;
-    }
-    if (selection > 80 && selection < 100) {
-      wallet[wallet.length] = 1;
-      i+=1;
+  let changeT = 0;
+  let pocketChange = [];
+  this.billAmount = 0;
+  this.coinList = [];
+  if (ranNum(1, 3) == 1) {
+    console.log("Hello," + Math.floor(parseFloat(salesPrice.salesPrice)));
+    let num = Math.floor(parseFloat(salesPrice.salesPrice));
+    changeT = 3;
+
+    for (let i = num; i > 0;) {
+    console.log(num);
+    console.log(this.billList);
+    console.log(i%100);
+    console.log(this.billAmount);
+    if (i % 100 == 0) {
+            this.billList[this.billList.length] = "hundredDollar.png.png";
+      i -= 100;
+      this.billAmount += 100;  
+    } else {
+      if (i % 50 == 0) {
+        this.billList[this.billList.length] = "fiftyDollar.png";
+        i -= 50;
+        this.billAmount += 50;
+      } else {
+        if (i % 20 == 0) {
+          this.billList[this.billList.length] = "twentyDollar.png";
+          i -= 20;
+          this.billAmount += 20;
+        } else {
+          if (i % 10 == 0) {
+            this.billList[this.billList.length] = "tenDollar.png";
+            i -= 10;
+            this.billAmount += 10;
+          } else {
+            if (i % 5 == 0) {
+              this.billList[this.billList.length] = "fiveDollar.png";
+              i -= 5;
+              this.billAmount += 5;
+            } else {
+              if (i % 1 == 0) {
+                this.billList[this.billList.length] = "oneDollar.png";
+                i -= 1;
+                this.billAmount += 1;
+              } else {
+              }
+            }
+          }
+        }
+      }
     }
   }
 
-  console.log(wallet);
-  console.log(salesPrice.salesPrice);
 
-let trimmedWallet = []; //just take the greatest of the bills until we hit salesPrice;s
- for (let i = 0; i <= salesPrice.salesPrice;) {
-  selection = ranNum(0,wallet.length);
+  for (let i = 0; i < changeT; ) {
     console.log(i);
-  if (wallet[selection] == 100) {
-    trimmedWallet[trimmedWallet.length] = 100;
-    i+=100;
+    i = parseFloat((i).toFixed(2));
+      selection = ranNum(0, 100);
+      if (selection > 0 && selection < 15) {
+        pocketChange[pocketChange.length] = 0.25;
+        i += 0.25;
+      }
+      if (selection > 20 && selection < 50) {
+        pocketChange[pocketChange.length] = 0.10;
+        i += 0.10;
+      }
+      if (selection > 50 && selection < 75) {
+        pocketChange[pocketChange.length] = 0.05;
+        i += 0.5;
+      }
+      if (selection > 75 && selection < 100) {
+        pocketChange[pocketChange.length] = 0.01;
+        i += 0.01;
+      }
+
+    }
+    for (let i = num; i <= salesPrice.salesPrice.toFixed(2); ) {
+      if (pocketChange.length == 0) {
+        for (let i = 0; i < changeT; ) {
+    console.log(i);
+    i = parseFloat((i).toFixed(2));
+      selection = ranNum(0, 100);
+      if (selection > 0 && selection < 15) {
+        pocketChange[pocketChange.length] = 0.25;
+        i += 0.25;
+      }
+      if (selection > 20 && selection < 50) {
+        pocketChange[pocketChange.length] = 0.10;
+        i += 0.10;
+      }
+      if (selection > 50 && selection < 75) {
+        pocketChange[pocketChange.length] = 0.05;
+        i += 0.5;
+      }
+      if (selection > 75 && selection < 100) {
+        pocketChange[pocketChange.length] = 0.01;
+        i += 0.01;
+      }
+
+    }
+      }
+      let maxIndex = pocketChange.indexOf(Math.max(...pocketChange));
+
+      if (pocketChange[maxIndex] == 0.25) {
+        this.coinList[this.coinList.length] = "quarter.png";
+        i += 0.25;
+        this.billAmount += 0.25;
+      }
+      if (pocketChange[maxIndex] == 0.10) {
+        this.coinList[this.coinList.length] = "dime.png";
+        i += 0.10;
+        this.billAmount += 0.10;
+      }
+      if (pocketChange[maxIndex] == 0.05) {
+        this.coinList[this.coinList.length] = "nickel.png";
+        i += 0.05;
+        this.billAmount += 0.05;
+      }
+      if (pocketChange[maxIndex] == 0.01) {
+        this.coinList[this.coinList.length] = "penny.png";
+        i += 0.01;
+        this.billAmount += 0.01;
+      }
+      pocketChange.splice(maxIndex, 1);
+    }
+   console.log(this.coinList);
+  } else {
+    for (let i = 0; i <= maxWallet; ) {
+      selection = ranNum(0, 100);
+      if (selection > 0 && selection < 5) {
+        wallet[wallet.length] = 100;
+        i += 100;
+      }
+      if (selection > 5 && selection < 15) {
+        wallet[wallet.length] = 50;
+        i += 50;
+      }
+      if (selection > 15 && selection < 40) {
+        wallet[wallet.length] = 20;
+        i += 20;
+      }
+      if (selection > 40 && selection < 65) {
+        wallet[wallet.length] = 10;
+        i += 10;
+      }
+      if (selection > 65 && selection < 80) {
+        wallet[wallet.length] = 5;
+        i += 5;
+      }
+      if (selection > 80 && selection < 100) {
+        wallet[wallet.length] = 1;
+        i += 1;
+      }
+    }
+
+    console.log(wallet);
+    console.log(salesPrice.salesPrice);
+
+    let trimmedWallet = []; //just take the greatest of the bills until we hit salesPrice;s
+    for (let i = 0; i <= salesPrice.salesPrice; ) {
+      selection = ranNum(0, wallet.length);
+      console.log(i);
+      if (wallet[selection] == 100) {
+        trimmedWallet[trimmedWallet.length] = 100;
+        i += 100;
+      }
+      if (wallet[selection] == 50) {
+        trimmedWallet[trimmedWallet.length] = 50;
+        i += 50;
+      }
+      if (wallet[selection] == 20) {
+        trimmedWallet[trimmedWallet.length] = 20;
+        i += 20;
+      }
+      if (wallet[selection] == 10) {
+        trimmedWallet[trimmedWallet.length] = 10;
+        i += 10;
+      }
+      if (wallet[selection] == 5) {
+        trimmedWallet[trimmedWallet.length] = 5;
+        i += 5;
+      }
+      if (wallet[selection] == 1) {
+        trimmedWallet[trimmedWallet.length] = 1;
+        i += 1;
+      }
+      console.log(i);
+    }
+
+    for (let i = 0; i <= salesPrice.salesPrice; ) {
+      let maxIndex = trimmedWallet.indexOf(Math.max(...trimmedWallet));
+
+      if (trimmedWallet[maxIndex] == 100) {
+        this.billList[this.billList.length] = "hundredDollar.png.png";
+        i += 100;
+        this.billAmount += 100;
+      }
+      if (trimmedWallet[maxIndex] == 50) {
+        this.billList[this.billList.length] = "fiftyDollar.png";
+        i += 50;
+        this.billAmount += 100;
+      }
+      if (trimmedWallet[maxIndex] == 20) {
+        this.billList[this.billList.length] = "twentyDollar.png";
+        i += 20;
+        this.billAmount += 20;
+      }
+      if (trimmedWallet[maxIndex] == 10) {
+        this.billList[this.billList.length] = "tenDollar.png";
+        i += 10;
+        this.billAmount += 10;
+      }
+      if (trimmedWallet[maxIndex] == 5) {
+        this.billList[this.billList.length] = "fiveDollar.png";
+        i += 5;
+        this.billAmount += 5;
+      }
+      if (trimmedWallet[maxIndex] == 1) {
+        this.billList[this.billList.length] = "oneDollar.png";
+        i += 1;
+        this.billAmount += 1;
+      }
+      trimmedWallet.splice(maxIndex, 1);
+    }
+  
+ this.billAmount = 0;
+ for (let i = 0; i < this.billList.length; i++) {
+  if (this.billList[i] == "hundredDollar.png.png") {
+    this.billAmount+=100;
   }
-  if (wallet[selection] == 50) {
-    trimmedWallet[trimmedWallet.length] = 50;
-    i+=50;
+  if (this.billList[i] == "fiftyDollar.png") {
+    this.billAmount+=50;
   }
-  if (wallet[selection] == 20) {
-    trimmedWallet[trimmedWallet.length] = 20;
-    i+=20;
+  if (this.billList[i] == "twentyDollar.png") {
+    this.billAmount+=20;
   }
-  if (wallet[selection] == 10) {
-    trimmedWallet[trimmedWallet.length] = 10;
-    i+=10;
+  if (this.billList[i] == "tenDollar.png") {
+    this.billAmount+=10;
   }
-  if (wallet[selection] == 5) {
-    trimmedWallet[trimmedWallet.length] = 5;
-    i+=5;
+  if (this.billList[i] == "fiveDollar.png") {
+    this.billAmount+=5;
   }
-  if (wallet[selection] == 1) {
-    trimmedWallet[trimmedWallet.length] = 1;
-    i+=1;
+  if (this.billList[i] == "oneDollar.png") {
+    this.billAmount+=1;
   }
-   console.log(i);
- }
-
-
-
-
- for (let i = 0; i <= salesPrice.salesPrice;) {
-let maxIndex = trimmedWallet.indexOf(Math.max(...trimmedWallet));
-
-if (trimmedWallet[maxIndex] == 100) {
-   this.billList[this.billList.length] = "hundredDollar.png.png";
-   i+=100;
 }
-if (trimmedWallet[maxIndex] == 50) {
-   this.billList[this.billList.length] = "fiftyDollar.png";
-   i+=50;
-}
-if (trimmedWallet[maxIndex] == 20) {
-   this.billList[this.billList.length] = "twentyDollar.png";
-   i+=20;
-}
-if (trimmedWallet[maxIndex] == 10) {
-   this.billList[this.billList.length] = "tenDollar.png";
-   i+=10;
-}
-if (trimmedWallet[maxIndex] == 5) {
-   this.billList[this.billList.length] = "fiveDollar.png";
-   i+=5;
-}
-if (trimmedWallet[maxIndex] == 1) {
-   this.billList[this.billList.length] = "oneDollar.png";
-   i+=1;
-}
- trimmedWallet.splice(maxIndex,1);
- }
+ console.log(this.billList);
 
- console.log(trimmedWallet);
-
-
-  this.change = parseFloat((this.billAmount - salesPrice.salesPrice).toFixed(2));
+console.log(this.billAmount + "hi");
+  }  
+this.change = parseFloat((this.billAmount - salesPrice.salesPrice).toFixed(2));
 
 },
 realRender: function(){
@@ -290,6 +469,25 @@ realRender: function(){
 
     
     imageRender(this.billList[i],num1,num2);
+        num1+=this.billW;
+        
+  }
+   num1 = this.startingW;
+  num2 = 0;
+  for (let i = 0; i < this.coinList.length; i++) {
+    if (num1 >= canvas.width * 0.90) {
+      num1 = 0;
+      if (document.getElementById("salesbox").offsetHeight > this.billH) {
+        num2+=document.getElementById("salesbox").offsetHeight;
+      }
+      else {
+        num2+= this.billH;
+      }
+
+    }
+
+    
+    imageRender(this.coinList[i],num1,num2);
         num1+=this.billW;
   }
 }
@@ -392,7 +590,6 @@ money.renderBills();
      screenColor.screenUpdate();
       money.realRender();
       console.log(money.change);
-
 
     //// End of loop
   };
